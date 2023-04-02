c32a42020749b1bafe3ec010d217e22a72850bcc
Samad <samadafzal1@g.ucla.edu>
2023-04-02 11:08:53 -0700

    added space for middlware parser and for mounting route frm controller

--- server/server.js
+++ server/server.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//when controller is up and ready
+const routes = require('./controller')
+
+// Middleware for parsing JSON data
+app.use(express.json());
+
+// Mounting the routes
+app.use('/', routes);
+
+// Starting the server
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
