e0a280ae264574a0e8246c9c9a23437740bd7ee8
Ben McIntyre <ben@Bens-MBP.fios-router.home>
2023-04-01 21:40:48 -0700

    Updates to Navbar styling, created hamburger button for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import classes from '../styles/Navbar.module.scss';
+import {FaBars} from 'react-icons/fa';
 
 const Navbar = () => {
+  const [showMobileNav, setShowMobileNav] = useState(false);
+
+  const toggleMobileNav = () => {
+    setShowMobileNav(!showMobileNav);
+  };
+
   return (
-    <nav>
+    <nav
+      className={`${classes.nav} ${
+        showMobileNav ? classes.showMobileNav : ''
+      }`}>
       <div className={classes.navContainer}>
         <Link
           to="/"
@@ -48,13 +58,48 @@ const Navbar = () => {
             <p className={classes.navName}>CONTACT</p>
           </button>
         </Link>
-        <Link
-          to="/donorDashboard"
-          className={classes.navLink}>
-          <button className={classes.navButton}>
-            <p className={classes.navName}>DONOR DASHBOARD</p>
-          </button>
-        </Link>
+        <div
+          className={classes.menuIcon}
+          onClick={toggleMobileNav}>
+          <FaBars />
+        </div>
+      </div>
+      <div
+        className={classes.overlay}
+        onClick={toggleMobileNav}></div>
+      <div className={classes.mobileNav}>
+        <div className={classes.mobileNavMenu}>
+          <Link
+            to="/"
+            className={classes.mobileNavLink}>
+            <p className={classes.navName}>HOME</p>
+          </Link>
+          <Link
+            to="/about"
+            className={classes.mobileNavLink}>
+            <p className={classes.navName}>ABOUT US</p>
+          </Link>
+          <Link
+            to="/adopt"
+            className={classes.mobileNavLink}>
+            <p className={classes.navName}>ADOPT</p>
+          </Link>
+          <Link
+            to="/foster"
+            className={classes.mobileNavLink}>
+            <p className={classes.navName}>FOSTER</p>
+          </Link>
+          <Link
+            to="/howToHelp"
+            className={classes.mobileNavLink}>
+            <p className={classes.navName}>HOW TO HELP</p>
+          </Link>
+          <Link
+            to="/contact"
+            className={classes.mobileNavLink}>
+            <p className={classes.navName}>CONTACT</p>
+          </Link>
+        </div>
       </div>
     </nav>
   );
