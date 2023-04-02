46c76cc7390a9b7e5ae397ac8e7b9bf584cfce93
Samad <samadafzal1@g.ucla.edu>
2023-04-02 12:26:35 -0700

    created user schema, included boolean for admin, also included pets and user relation - def need to double check

--- server/Models/modelSchema.js
+++ server/Models/modelSchema.js
@@ -16,3 +16,29 @@ const Pet = sequelize.define('Pet', {
       allowNull: false
     }
   });
+
+  // Define your User model schema.. but not sure if admin is supposed to work likle this
+const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    is_admin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+
+// Not sure if we have to define relationships here or if this would be considered right? 
+Pet.belongsTo(User);
+User.hasMany(Pet);
+
+// Sync your schema with your database
+sequelize.sync();
+
+module.exports = { Pet, User };
