3bb103c07112d2af61f059e8d16cef31ed960e8b
Ben Newman <108847653+bnewman27@users.noreply.github.com>
2023-04-11 20:39:57 -0700

    refactored with no errors

--- client/src/components/MailApp.jsx
+++ client/src/components/MailApp.jsx
@@ -1,46 +1,46 @@
-import React from 'react'; 
-import Mailbox from './Mailbox';
-import MailboxList from './MailboxList';
+import React, { Component } from 'react';
 import NoneSelected from './NoneSelected';
+import Email from './Email';
+import EmailList from './EmailList';
 
+class Mailbox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { email_id: null };
+    this.handleSelectEmail = this.handleSelectEmail.bind(this);
+  }
 
+  handleSelectEmail(id) {
+    this.setState({ email_id: id });
+  }
 
-var MailApp = React.createClass({
-    getInitialState: function () {
-      return { mailbox_id: null }
-    },
-  
-    handleSelectMailbox: function (id) {
-      this.setState({ mailbox_id: id })
-    },
-  
-    render: function () {
-      var mailbox_id = this.state.mailbox_id
-      if (mailbox_id) {
-        var mailbox = this.props.mailboxes.filter(function (mailbox) {
-          return mailbox.id == mailbox_id
-        })[0]
-        selected_mailbox = <Mailbox key={mailbox.id} emails={mailbox.emails} />
-      } else {
-        selected_mailbox = <NoneSelected text="mailbox" />
-      }
-  
-      return (
-        <div className="app row">
-          <MailboxList
-            mailboxes={this.props.mailboxes}
-            onSelectMailbox={this.handleSelectMailbox}
-          />
-          <div className="mailbox col-md-10">
-            <div className="panel panel-default">
-              <div className="panel-body">{selected_mailbox}</div>
-            </div>
-          </div>
-        </div>
-      )
-    },
-  })
+  render() {
+    const { email_id } = this.state;
+    const { emails } = this.props;
+    let selected_email;
 
-  export default MailApp;
+    if (email_id) {
+      const mail = emails.find((mail) => mail.id === email_id);
+      selected_email = (
+        <Email
+          id={mail.id}
+          from={mail.from}
+          to={mail.to}
+          subject={mail.subject}
+          body={mail.body}
+        />
+      );
+    } else {
+      selected_email = <NoneSelected text="email" />;
+    }
 
-  
+    return (
+      <div>
+        <EmailList emails={emails} onSelectEmail={this.handleSelectEmail} />
+        <div className="email-viewer">{selected_email}</div>
+      </div>
+    );
+  }
+}
+
+export default Mailbox;
--- client/src/components/Mailbox.jsx
+++ client/src/components/Mailbox.jsx
@@ -1,47 +1,46 @@
-import React from 'react'; 
+import React, { Component } from 'react';
 import NoneSelected from './NoneSelected';
 import Email from './Email';
 import EmailList from './EmailList';
 
+class Mailbox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { email_id: null };
+    this.handleSelectEmail = this.handleSelectEmail.bind(this);
+  }
 
-var Mailbox = React.createClass({
-    getInitialState: function () {
-      return { email_id: null }
-    },
-  
-    handleSelectEmail: function (id) {
-      this.setState({ email_id: id })
-    },
-  
-    render: function () {
-      var email_id = this.state.email_id
-      if (email_id) {
-        var mail = this.props.emails.filter(function (mail) {
-          return mail.id == email_id
-        })[0]
-        selected_email = (
-          <Email
-            id={mail.id}
-            from={mail.from}
-            to={mail.to}
-            subject={mail.subject}
-            body={mail.body}
-          />
-        )
-      } else {
-        selected_email = <NoneSelected text="email" />
-      }
-  
-      return (
-        <div>
-          <EmailList
-            emails={this.props.emails}
-            onSelectEmail={this.handleSelectEmail}
-          />
-          <div className="email-viewer">{selected_email}</div>
-        </div>
-      )
-    },
-  })
+  handleSelectEmail(id) {
+    this.setState({ email_id: id });
+  }
 
-  export default Mailbox;
+  render() {
+    const { email_id } = this.state;
+    const { emails } = this.props;
+    let selected_email;
+
+    if (email_id) {
+      const mail = emails.find((mail) => mail.id === email_id);
+      selected_email = (
+        <Email
+          id={mail.id}
+          from={mail.from}
+          to={mail.to}
+          subject={mail.subject}
+          body={mail.body}
+        />
+      );
+    } else {
+      selected_email = <NoneSelected text="email" />;
+    }
+
+    return (
+      <div>
+        <EmailList emails={emails} onSelectEmail={this.handleSelectEmail} />
+        <div className="email-viewer">{selected_email}</div>
+      </div>
+    );
+  }
+}
+
+export default Mailbox;
