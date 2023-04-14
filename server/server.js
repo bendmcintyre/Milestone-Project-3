74c3e1ca8a812e8a6e27ef54585ba9e639774101
Ben Newman <108847653+bnewman27@users.noreply.github.com>
2023-04-13 20:12:52 -0700

    merge catch up

--- server/server.js
+++ server/server.js
@@ -22,6 +22,10 @@ app.post('/api/contact', (req, res) => {
   // Handle form submission logic here
   console.log(req.body);
   res.sendStatus(200);
+
+// Protected route for user's profile
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
 });
 
 // Starting the server
