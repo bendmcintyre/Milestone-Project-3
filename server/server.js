fa0d796eac334fe4cdb6952177a1e681262017de
Ben McIntyre <ben@Bens-MBP.fios-router.home>
2023-04-13 19:39:15 -0700

    Updates to Auth0 and backend on auth.js and server.js

--- server/server.js
+++ server/server.js
@@ -2,21 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//Auth0
-const { sessionMiddleware, authMiddleware } = require('./auth');
+// Auth0
+const { sessionMiddleware, authMiddleware, requiresAuth } = require('./auth');
 
 app.use(sessionMiddleware);
 app.use(authMiddleware);
 
-//When controller is up and ready
-
-const routes = require('./routes')
+// When controller is up and ready
+const routes = require('./routes');
 
 // Middleware for parsing JSON data
 app.use(express.json());
 
 // Mounting the routes
 app.use('/', routes);
+
+// Protected route for user's profile
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 // Starting the server
 app.listen(PORT, () => {
